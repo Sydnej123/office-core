6cf74395999f0e6187b589be28bdf30c11869a7f
mzbiewski <00zbiewski@gmail.com>
2023-11-09 17:32:45 +0100

    Init

---
+++ src/main/java/com/ttsw/officecore/OfficeCoreApplication.java
@@ -0,0 +1,13 @@
+package com.ttsw.officecore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficeCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfficeCoreApplication.class, args);
+	}
+
+}
